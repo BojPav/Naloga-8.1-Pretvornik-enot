5a65a0f87001b79b0d3388c4d6b201f1fb6ff312
Bojan Pavlovic <pavlovicbojan86@gmail.com>
2016-09-23 22:06:22 +0200

    Update Pretvornik_enot.py

--- Pretvornik_enot.py
+++ Pretvornik_enot.py
@@ -3,7 +3,7 @@ print "Pozdravljeni ! Welcome to -PRETVORNIK ENOT-( Kmh --> Mph )"
 
 while True:
     kmh = int(raw_input("Vnesite število kilometrov v kmh: "))
-    mph = float(1.6) * kmh
+    mph = float(0.621371) * kmh
     print mph
     vprasanje = raw_input("Ali želite narediti novo pretvorbo ?")
     if vprasanje == "ne":
